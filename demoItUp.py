9a22089c95f87d899feef02b2bc119d2e36963e7
SArehalli <sgarehal@ucsd.edu>
2015-11-16 16:54:22 -0800

    Reorganized for live walkthrough with kids

--- demoItUp.py
+++ demoItUp.py
@@ -5,61 +5,81 @@ from struct import unpack
 import wave
 import audioop
 
-sampling_rate = 44100
-num_channels = 2
-chunk_size = 512
-data_out = aa.PCM(aa.PCM_PLAYBACK, aa.PCM_NORMAL)
-data_out.setchannels(num_channels)
-data_out.setrate(sampling_rate)
-data_out.setformat(aa.PCM_FORMAT_S16_LE)
-data_out.setperiodsize(chunk_size)
+BOARD_SIZE = 32
 
-d = Display(32, 32)
+def normalize(data):
+    """ limit the range of an array to be between 0 and BOARD_SIZE """
+    norm_matrix = np.int_(np.log10(data)**2)
+    norm_matrix = map(lambda x: x if x < BOARD_SIZE else BOARD_SIZE, norm_matrix)
+    norm_matrix = map(lambda x: x if x > 0 else 0, norm_matrix)
+    return norm_matrix
+    
 
+def transform_to_levels(data, chunk, sampling_rate):
+    # Convert the data into a numpy array 
+    data = np.fromstring(data, dtype= np.int16)
+    
+    # Grab one channel
+    fourier = data[0::2]
+
+    # Fourier Transform
+    fourier = np.abs(np.fft.rfft(data)[:-1])
+
+    # make bins
+    matrix = np.reshape(fourier,(BOARD_SIZE, chunk/BOARD_SIZE))
+    
+    # Get the maxs, averages, and mins for the bins
+    levels_min = normalize(np.min(data,1))
+    levels_avg = normalize(np.average(data,1))
+    levels_max = normalize(np.max(data,1))
+
+    # return those maxs, averages, and mins
+    return levels_max, levels_avg, levels_min
+
+
+# Get user input
 a = raw_input()
 sound = wave.open('./' + a +'.wav', 'r')
 
-def transform_to_levels(data, chunk, sampling_rate):
-    data = np.fromstring(data, dtype= np.int16)
-    fourier = data[0::2]
-    fourier = np.fft.rfft(data)[:-1]
-    fourier = (np.abs(fourier))
 
-    data = np.reshape(fourier,(32, chunk/32))
-    
-    power_min = (np.log10(np.min(data,1))**2)
-    matrix_min = np.int_(power_min)
-    matrix_min = map(lambda x: x if x < 32 else 32, matrix_min)
-    matrix_min = map(lambda x: x if x > 0 else 0, matrix_min)
+# Setup audio output stream
+data_out = aa.PCM(aa.PCM_PLAYBACK, aa.PCM_NORMAL)
 
-    power_avg = (np.log10(np.average(data,1))**2)
-    matrix_avg = np.int_(power_avg)
-    matrix_avg = map(lambda x: x if x < 32 else 32, matrix_avg)
-    matrix_avg = map(lambda x: x if x > 0 else 0, matrix_avg)
+num_channels = sound.getnchannels()
+data_out.setchannels(num_channels)
 
-    power_max = (np.log10(np.max(data,1))**2)
-    matrix_max = np.int_(power_max)
-    matrix_max = map(lambda x: x if x < 32 else 32, matrix_max)
-    matrix_max = map(lambda x: x if x > 0 else 0, matrix_max)
-    
-    return matrix_max, matrix_avg,  matrix_min
+sampling_rate = sound.getframerate()
+data_out.setrate(sampling_rate)
 
+data_out.setformat(aa.PCM_FORMAT_S16_LE)
+
+chunk_size = 512
+data_out.setperiodsize(chunk_size)
+
+
+# Create a display object
+d = Display(BOARD_SIZE, BOARD_SIZE)
+
+# Read a chunk's worth of data
 data = sound.readframes(chunk_size)
-while(data!=''):
-    try:
-        data_out.write(data) 
-        levels_max, levels_avg, levels_min = transform_to_levels(data, 
-                                                                 chunk_size, 
-                                                                 sampling_rate)
-        d.clear()
-        for i in range(32):
-            d.drawSquare(i,0,1, levels_max[31-i], 0xff0000)
-            d.drawSquare(i,0,1, levels_avg[31-i], 0xff00ff)
-            d.drawSquare(i,0,1, levels_min[31-i], 0x0000ff)
-        data = sound.readframes(chunk_size)
-    
-    except Exception :
-        d.clear()
-    
 
+# While music is playing
+while(data != ''):
+    # play music  
+    data_out.write(data)
 
+    # Get the max, min, and average levels
+    levels_max, levels_avg, levels_min = transform_to_levels(data, 
+                                                             chunk_size, 
+                                                             sampling_rate)
+    # Clear the board
+    d.clear()
+
+    # Redraw the board
+    for i in range(BOARD_SIZE):
+        d.drawSquare(i, 0, 1, levels_max[31-i], 0xff0000)
+        d.drawSquare(i, 0, 1, levels_avg[31-i], 0xff00ff)
+        d.drawSquare(i, 0, 1, levels_min[31-i], 0x0000ff)
+    
+    # Read more data
+    data = sound.readframes(chunk_size)
